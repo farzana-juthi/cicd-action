39e174d0ece5f89cf43065c3c3e164aa799a8b71
farzana-juthi <bimjuthi@gmail.com>
2024-02-25 16:21:17 +0600

    check ksuid

--- hello_world/app.py
+++ hello_world/app.py
@@ -42,7 +42,7 @@ def lambda_handler(event, context):
     #     print(e)
 
     #     raise e
-    print(str(ksuid()))
+    print("ksuid ",str(ksuid()))
     return {
         "statusCode": 200,
         "body": json.dumps({
